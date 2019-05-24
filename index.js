95faf8e196d00eb9c9ed802fbecb0787da4ff5b0
Joseph Andrews <andrjose@flip2.engr.oregonstate.edu>
2019-05-23 23:43:51 -0700

    added js for menu selection and stored in sessionStorage

---
+++ index.js
@@ -0,0 +1,22 @@
+var solveButton = document.getElementById("solveButton");
+solveButton.onclick = function() {printOptions()};
+
+function printOptions() {
+    var selectCount = 0;
+    var bombId = "";
+    for (var i = 0; i < document.getElementsByTagName("select").length; i++) {
+        var option = document.getElementsByTagName("select")[i].value;
+        //console.log(option);
+        if (option === "select")
+            selectCount++;
+        else
+            bombId = option;
+    }
+    if(selectCount === 6) {
+        console.log("BombID: ", bombId);
+        sessionStorage.setItem("bombId", bombId);
+    } else
+        alert("Choose exactly 1 bomb");
+}
+
+//sessionStorage.setItem("storedVal", 69);
