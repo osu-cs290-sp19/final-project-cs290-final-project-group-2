249fd80442df5bbda0351a8ebb81b013239d3d1c
Ryan Russell <russelry@flip2.engr.oregonstate.edu>
2019-05-24 12:55:10 -0700

    Fixed simple_wires.js file to display create twit when the simple wires button is pressed

--- modules/simple_wires.js
+++ modules/simple_wires.js
@@ -1,7 +1,8 @@
 console.log("hey");
-document.getElementById("simple-wires").addEventListener("click", display_create_twit);
+
+window.onload = function() {document.getElementById("simple-wires").addEventListener("click", display_create_twit);}
 
 function display_create_twit() {
-  document.getElementById("modal-backdrop").style.display = "block";
-  document.getElementById("create-twit-modal").style.display = "block";
+    document.getElementById("modal-backdrop").classList.remove("hidden");
+    document.getElementById("create-twit-modal").classList.remove("hidden");
 }
