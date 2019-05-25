b803861008c2ead6a9933bcf9b0f47cbdc0c3279
Joseph Andrews <GAndrews98@github.com>
2019-05-24 22:10:45 -0700

    Added module form for button and simple wires

--- modules.js
+++ modules.js
@@ -1,1 +1,12 @@
 console.log("Value retrieved:", sessionStorage.getItem("bombId"));
+
+
+window.onload = function() {
+    document.getElementById("simple-wires").addEventListener("click", display_simple_wire_module);
+    var simpleWires = document.getElementById("simple-wires-modal");
+    simpleWires.addEventListener("click", testWires);
+    document.getElementById("button").addEventListener("click", display_button_module);
+    var theButton = document.getElementById("the-button-modal");
+    theButton.addEventListener("click", testButton);
+    console.log("event listeners added");
+}
