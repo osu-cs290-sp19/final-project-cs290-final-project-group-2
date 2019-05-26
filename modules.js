3f4ab0bfdbe4fbfcfeeba2a999b127e860345c45
Ryan Russell <russelry@flip2.engr.oregonstate.edu>
2019-05-25 20:21:19 -0700

    simple_wires.js is complete

--- modules.js
+++ modules.js
@@ -2,11 +2,11 @@ console.log("Value retrieved:", sessionStorage.getItem("bombId"));
 
 
 window.onload = function() {
+    // Simple Wires
     document.getElementById("simple-wires").addEventListener("click", display_simple_wire_module);
-    var simpleWires = document.getElementById("simple-wires-modal");
-    simpleWires.addEventListener("click", testWires);
+    document.getElementById("simple-wires-modal").addEventListener("click", check_simple_wires_button);
+
+    //The Button
     document.getElementById("button").addEventListener("click", display_button_module);
-    var theButton = document.getElementById("the-button-modal");
-    theButton.addEventListener("click", testButton);
-    console.log("event listeners added");
+    document.getElementById("the-button-modal").addEventListener("click", testButton);
 }
