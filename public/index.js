648d865555bc15a00bd1c619057440045eecefdc
Gatlin Andrews <gatlin98@gmail.com>
2019-06-02 15:29:12 -0700

    Added basic login functionality

--- public/index.js
+++ public/index.js
@@ -1,6 +1,9 @@
 var solveButton = document.getElementById("solveButton");
 solveButton.onclick = function() {checkLevels()};
 
+var loginButton = document.getElementById("navbar-user-button");
+loginButton.onclick = function() {get_user()};
+
 function checkLevels() {
     var selectCount = 0;
     var bombId = "";
@@ -20,4 +23,10 @@ function checkLevels() {
         alert("Choose exactly 1 bomb");
 }
 
+function get_user() {
+    var username = document.getElementById('navbar-user-input').value;
+    alert("Welcome " + username + "! You are now signed in.");
+    sessionStorage.setItem("username", username);
+    console.log(username);
+}
 //sessionStorage.setItem("storedVal", 69);
