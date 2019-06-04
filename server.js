6946c25c9995b3bee527500c7013a53e9e476ece
Alexander Rash <rasha@flip3.engr.oregonstate.edu>
2019-06-04 14:36:11 -0700

    git ignore added. server modified slightly

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
+const path = require('path');
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
-app.listen(3004, () => console.log('Listening at 3004'));
+var port = process.env.PORT || 3001 || 3333;
 app.use(express.static('public'));
+
+
+// app.use(function(req,res,next){
+//   res.status(404).send("Sorry, that page does not exist");
+// });
+app.post('/stats/update', function (req, res)){
+
+});
+app.get('*', function (req, res) {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+app.listen(port, () => console.log('Listening on port ', port));
