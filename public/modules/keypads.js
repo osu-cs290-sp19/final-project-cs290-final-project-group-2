1bc4858dcf6099f521ffc6aa446fd5a03234d362
Gatlin Andrews <gatlin98@gmail.com>
2019-06-28 23:56:32 -0700

    Revert "Added a bunch of stuff. Like a lot"

--- public/modules/keypads.js
+++ public/modules/keypads.js
@@ -1,10 +1,1 @@
 //TODO
-function display_keypads_module() {
-    document.getElementById("modal-backdrop").classList.remove("hidden");
-    document.getElementById("keypads-modal").classList.remove("hidden");
-}
-
-function hide_keypads_module() {
-    document.getElementById("modal-backdrop").classList.add("hidden");
-    document.getElementById("keypads-modal").classList.add("hidden");
-}
